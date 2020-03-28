f665105686d77251904c6e928b0d502a7db63255
Joe Martella <joem@microsoft.com>
2020-03-28 16:52:29 -0700

    Adds socket

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import io from "socket.io-client";
 
 function App() {
   const [roomCode, setRoomCode] = useState(0);
   const [name, setName] = useState("");
   const [joinRoomCode, setJoinRoomCode] = useState("");
+
+  let socket = null;
+  React.useEffect(() => {
+    socket = io("http://localhost:3000");
+    return socket.close();
+  }, []);
 
   const onCreateRoomClick = async () => {
     const response = await fetch(
