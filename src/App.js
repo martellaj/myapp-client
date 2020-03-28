45e270e1e25dc8e70c8cabd57a49adef11b0b1e4
Joe Martella <joem@microsoft.com>
2020-03-28 16:07:38 -0700

    creates join ui

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import "./App.css";
 
 function App() {
   const [roomCode, setRoomCode] = useState(0);
+  const [name, setName] = useState("");
+  const [joinRoomCode, setJoinRoomCode] = useState("");
 
   const onCreateRoomClick = async () => {
     const response = await fetch(
-      "http://localhost:3000/room/create/playerName/joe/",
+      `http://localhost:3000/room/create/playerName/${name}/`,
       {
         method: "POST"
       }
@@ -15,7 +17,22 @@ function App() {
     if (response.status == 200) {
       const parsedResponse = await response.json();
 
-      console.log(parsedResponse);
+      if (parsedResponse.joined) {
+        setRoomCode(parsedResponse.roomCode);
+      }
+    }
+  };
+
+  const onJoinRoomClick = async () => {
+    const response = await fetch(
+      `http://localhost:3000/room/join/${joinRoomCode}/playerName/${name}/`,
+      {
+        method: "POST"
+      }
+    );
+
+    if (response.status == 200) {
+      const parsedResponse = await response.json();
 
       if (parsedResponse.joined) {
         setRoomCode(parsedResponse.roomCode);
@@ -23,16 +40,52 @@ function App() {
     }
   };
 
-  const onJoinRoomClick = () => {};
-
   const content =
     roomCode > 0 ? (
       <p>your in room {roomCode}</p>
     ) : (
-      <>
-        <button onClick={onCreateRoomClick}>create room</button>
-        <button>join room</button>
-      </>
+      <div className="joinRoom">
+        <input
+          placeholder="name"
+          value={name}
+          onChange={event => setName(event.target.value)}
+          style={{
+            marginBottom: "12px"
+          }}
+        />
+        <div style={{ display: "flex" }}>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              marginRight: "24px"
+            }}
+          >
+            create a new room
+            <button
+              onClick={onCreateRoomClick}
+              disabled={!name}
+              style={{
+                marginBottom: "12px"
+              }}
+            >
+              create
+            </button>
+          </div>
+          <div style={{ display: "flex", flexDirection: "column" }}>
+            join a room
+            <input
+              placeholder="1234"
+              value={joinRoomCode}
+              onChange={event => setJoinRoomCode(event.target.value)}
+              style={{ marginTop: "6px", marginBottom: "6px" }}
+            />
+            <button onClick={onJoinRoomClick} disabled={!joinRoomCode}>
+              join
+            </button>
+          </div>
+        </div>
+      </div>
     );
 
   return <div className="App">{content}</div>;
