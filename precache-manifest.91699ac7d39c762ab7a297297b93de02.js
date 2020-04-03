5dc35362c12dfbbfd6de0a9db99f9c917b200915
Joe Martella <joem@microsoft.com>
2020-04-02 21:29:44 -0700

    Updates

---
+++ precache-manifest.91699ac7d39c762ab7a297297b93de02.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18603d1018fcdfe51cb2b7766f24dfc6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd78c6e3f30bec406f2b",
+    "url": "/static/css/2.77b93774.chunk.css"
+  },
+  {
+    "revision": "3b77246e05162dcc8925",
+    "url": "/static/css/main.b2e56da4.chunk.css"
+  },
+  {
+    "revision": "dd78c6e3f30bec406f2b",
+    "url": "/static/js/2.feacde7e.chunk.js"
+  },
+  {
+    "revision": "bab979f0996c4f83a712fc8810462bad",
+    "url": "/static/js/2.feacde7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b77246e05162dcc8925",
+    "url": "/static/js/main.5768ea0b.chunk.js"
+  },
+  {
+    "revision": "68e4002a58cb9f66f3dd",
+    "url": "/static/js/runtime-main.8a780217.js"
+  }
+]);
